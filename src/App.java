e8bd8cde33ab67b690c59a034a6fb141515016f6
LeoValdi <leonardovaldivia2004@gmail.com>
2023-12-04 00:40:04 -0500

    Se crea el exel en javafx

--- src/App.java
+++ src/App.java
@@ -1,19 +1,82 @@
 import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
-public class App extends Application{
-    
+public class App extends Application {
+
     @Override
     public void start(Stage primaryStage) throws Exception {
-        
-        
+
+        int numFilas = 10;
+        int numColumnas = 10;
+
+        TextField[][] celdas = new TextField[numFilas][numColumnas];
+        GridPane grid = new GridPane();
+
+        // Añadir letras en la parte superior
+        for (int j = 0; j < numColumnas; j++) {
+            Label label = new Label(Character.toString((char) ('A' + j)));
+            label.setAlignment(Pos.CENTER);
+            label.setMaxWidth(Double.MAX_VALUE); // Ajusta este valor según el tamaño de tus celdas
+            label.setMinHeight(20); // Ajusta este valor según el tamaño de tus celdas
+            GridPane.setRowIndex(label, 0);
+            GridPane.setColumnIndex(label, j + 1);
+            grid.getChildren().add(label);
+        }
+
+        // Añadir números a la izquierda
+        for (int i = 0; i < numFilas; i++) {
+            Label label = new Label(Integer.toString(i + 1));
+            label.setAlignment(Pos.CENTER);
+            label.setMinWidth(20);
+            GridPane.setRowIndex(label, i + 1); // +1 para dejar espacio para las letras arriba
+            GridPane.setColumnIndex(label, 0);
+            grid.getChildren().add(label);
+        }
+
+        // Añadir celdas
+        for (int i = 0; i < numFilas; i++) {
+            for (int j = 0; j < numColumnas; j++) {
+                TextField textField = new TextField();
+                textField.setOnKeyTyped(manejarEscritura);
+                GridPane.setRowIndex(textField, i + 1); // +1 por las letras
+                GridPane.setColumnIndex(textField, j + 1); // +1 por los números
+                celdas[i][j] = textField;
+                grid.getChildren().add(textField);
+            }
+        }
+
+
+        grid.setGridLinesVisible(true);
+        // Aquí puedes añadir más estilos según necesites
+
+        Scene scene = new Scene(grid, 800, Double.MAX_VALUE);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
     }
-    
+
+    EventHandler<KeyEvent> manejarEscritura = new EventHandler<KeyEvent>() {
+    @Override
+    public void handle(KeyEvent event) {
+        TextField textField = (TextField) event.getSource();
+        int fila = GridPane.getRowIndex(textField) - 1; // -1 porque la primera fila son las etiquetas
+        int columna = GridPane.getColumnIndex(textField) - 1; // -1 porque la primera columna son las etiquetasa
+        
+        System.out.println("Escribiendo en la celda: Fila " +( fila+1) + ", Columna " + (char)('A' + columna));
+        // Aquí puedes agregar más lógica según necesites
+    }
+};
+
+
     public static void main(String[] args) {
-        
-        
-        
-        
+        launch(args);
     }
-    
+
 }
