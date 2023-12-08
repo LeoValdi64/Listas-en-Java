3f8923eefa8379e4fe1a3924fe9d533f9e6c3899
LeoValdi <leonardovaldivia2004@gmail.com>
2023-12-08 02:58:51 -0500

    Una gran mejora, pero sigue con errores

--- src/Celda.java
+++ src/Celda.java
@@ -8,6 +8,7 @@ public class Celda {
         Node previous;
         Node up;
         Node down;
+
         Node(int row, int col, String data) {
             this.row = row;
             this.col = col;
@@ -17,7 +18,7 @@ public class Celda {
             this.up = null;
             this.down = null;
         }
-        
+
     }
 
     private Node celda;
@@ -29,49 +30,63 @@ public class Celda {
     public void add(int row, int col, String data) {
         Node temp = new Node(row, col, data);
 
-        if (celda.data == null) { // En el caso que sea la primera celda
+        if (celda == null) { // En el caso que sea la primera celda
             celda = temp;
             return;
         }
         Node ejeX = celda;
 
-        while (ejeX != null) {
-            if (ejeX.row == row && ejeX.col == col) {// En el caso que ya exista una celda en esa posicion
-                ejeX.data = data;
-                return;
-            }
+        if (ejeX.col > col) { // Si la columna es menor a la primera
+            temp.next = ejeX;
+            ejeX.previous = temp;
+            celda = temp;
+            return;
+        }
 
-            if (ejeX.col > col) {// En el caso que la columna deba ir al principio
-                temp.next = ejeX;
-                ejeX.previous = temp;
-                celda = temp;
-                return;
-            }
-
-            if (ejeX.next == null) {// En el caso que sea la ultima columna
+        while (ejeX.col != col) {
+            if (ejeX.next == null && col > ejeX.col) {// Si la columna no existe la crea
                 ejeX.next = temp;
                 temp.previous = ejeX;
                 return;
             }
-
-            if (ejeX.next != null) {
-                if (ejeX.col < col && ejeX.next.col > col) {// En el caso que la columna deba ir
-                                                              // en medio
-                    if (row < ejeX.row) {// En el caso que la fila deba ir al principio
-                        ejeX.up = temp;
-                        temp.down = ejeX;
-                        
-                        return;
-                    }
-                    ejeX.next.previous = temp;
-                    temp.next = ejeX.next;
-                    ejeX.next = temp;
-                    temp.previous = ejeX;
-                    return;
-
-                }
+            if (ejeX.col < col && ejeX.next.col > col) { // Si la columna esta en medio y no existe
+                temp.next = ejeX.next;
+                ejeX.next.previous = temp;
+                ejeX.next = temp;
+                temp.previous = ejeX;
+                return;
             }
             ejeX = ejeX.next;
+
+        }
+
+        Node ejeY = ejeX;
+        if (ejeY.row > row) { // Si la fila es menor a la primera
+            ejeY.up = temp;
+            temp.down = ejeY;
+            return;
+        }
+        while (ejeY.row != row) {
+
+            if (ejeY.down == null && row > ejeY.row) {// Si la fila no existe la crea
+                ejeY.down = temp;
+                temp.up = ejeY;
+                return;
+            }
+
+            if (ejeY.row < row && ejeY.down.row > row) { // Si la fila esta en medio y no existe
+                temp.down = ejeY.down;
+                ejeY.down.up = temp;
+                ejeY.down = temp;
+                temp.up = ejeY;
+                return;
+            }
+            ejeY = ejeY.down;
+        }
+
+        if (ejeX.col == col && ejeY.row == row) { // Si la celda ya existe
+            ejeY.data = data;
+            return;
         }
 
     }
@@ -81,22 +96,41 @@ public class Celda {
         Node temp = celda;
         String d = "";
         while (temp != null) {
+            d += "[ ";
             Node temp2 = temp;
-            d += ("(");
             while (temp2 != null) {
-                d += (temp2.data);
+                d += temp2.data;
                 temp2 = temp2.down;
                 if (temp2 != null) {
-                    d += (", ");
+                    d += ", ";
                 }
             }
-            d += (")");
+            d += " ] ";
             temp = temp.next;
-            if (temp != null) {
-                d += (", ");
-            }
         }
         return d;
     }
+    // @Override
+    // public String toString() {
+    // Node temp = celda;
+    // String d = "";
+    // while (temp != null) {
+    // Node temp2 = temp;
+    // d += ("(");
+    // while (temp2 != null) {
+    // d += (temp2.data);
+    // temp2 = temp2.down;
+    // if (temp2 != null) {
+    // d += (", ");
+    // }
+    // }
+    // d += (")");
+    // temp = temp.next;
+    // if (temp != null) {
+    // d += (", ");
+    // }
+    // }
+    // return d;
+    // }
 
 }
